66442d3afe5d445b0976a4845b9f6e7e869e56ce
sahtiwk <sathwikreddydudipala@gmail.com>
2025-12-19 18:18:13 +0530

    Routing setup

--- FoodProductExplorer/src/App.jsx
+++ FoodProductExplorer/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/product/:barcode" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
