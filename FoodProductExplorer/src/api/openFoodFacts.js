388dc4b24e282077842b82dc0fb7508a119d9023
sahtiwk <sathwikreddydudipala@gmail.com>
2025-12-19 18:16:57 +0530

    API design

--- FoodProductExplorer/src/api/openFoodFacts.js
+++ FoodProductExplorer/src/api/openFoodFacts.js
@@ -1,3 +1,31 @@
-const headers = {
-  "User-Agent": "FoodExplorer/1.0 (sathwikreddydudipala@gmail.com)"
-};
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://world.openfoodfacts.org/",
+    headers: {
+        "User-Agent": "FoodProductExplorer/1.0 (sathwikreddydudipala@gmail.com)"
+    }
+});
+
+export const searchProducts = async({query, page= 1, pageSize= 20}) => {
+    const res = await api.get("/cgi/search.pl", {
+        params: {
+            search_terms: query,
+            json: true,
+            page,
+            page_size: pageSize
+        }
+    });
+    return res.data.products;
+}
+
+export const getProductByCategory = async ({category, page=1}) => {
+    const res= await api.get("/api/v2/search", {
+        params: {
+            categories_tags_en: category,
+            page,
+            page_size: 20
+        }
+    });
+    return res.data.products;
+}
